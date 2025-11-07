aa09d0914e41034b5bb6d48c75504887331f81d4
Yanfei Xiang <xiangyanfei212@outlook.com>
2025-11-07 12:13:38 +0800

    Create averager.py

---
+++ utils/averager.py
@@ -0,0 +1,12 @@
+class Averager():
+
+    def __init__(self):
+        self.n = 0.0
+        self.v = 0.0
+
+    def add(self, v, n=1.0):
+        self.v = (self.v * self.n + v * n) / (self.n + n)
+        self.n += n
+
+    def item(self):
+        return self.v
